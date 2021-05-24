4be36890035ff1cd12915b5e90406156099b6eaf
Nikhil Karanjkar <nkaranjkar@equalexperts.com>
2021-05-24 20:23:27 +0530

    "Array - Remove All duplicates from Array in O(1) space"

---
+++ src/main/java/org/coding/arrays/removeAllDuplicatesFromSortedArray/GFG.java
@@ -0,0 +1,48 @@
+package org.coding.arrays.removeAllDuplicatesFromSortedArray;// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T>0)
+        {
+            int N = sc.nextInt();
+            int a[] = new int[N];
+            for(int i=0; i<N; i++)
+                a[i] = sc.nextInt();
+            
+            Solution g = new Solution();
+            int n = g.remove_duplicate(a,N);
+            
+            for(int i=0; i<n; i++)
+                System.out.print(a[i]+" ");
+            System.out.println();
+            T--;
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int remove_duplicate(int A[],int N){
+        // code here
+        int start = 0;
+
+        for(int i =1; i< N; i++)
+        {
+            if(A[i] != A[start])
+            {
+                start++;
+                A[start] = A[i];
+            }
+        }
+
+        return start+1;
+    }
+}
